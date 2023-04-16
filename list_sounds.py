f72a251332f3d8c88893e35fc9cf90eabc65f0ee
JMaleta <13309090+JMeta0@users.noreply.github.com>
2023-04-16 19:36:55 +0200

    add checking if sounds are compatible on load

--- list_sounds.py
+++ list_sounds.py
@@ -1,6 +1,17 @@
-from os import listdir
+import os
 import logging
 import sys
+from platform import system
+
+
+system = system()
+if system == 'Windows':
+    sox_path = r'sox'
+    os.environ['PATH'] = sox_path + ';' + os.environ['PATH']
+    import sox
+else:
+    import sox
+
 
 # Logs
 log = logging.getLogger()
@@ -17,9 +28,17 @@ logging.basicConfig(level=log_level, format="%(name)s - %(message)s", datefmt="%
 def list_sounds():
     sounds = []
     sounds_directory = "sounds"
-
-    for filename in listdir(sounds_directory):
+    print('Loading sounds...')
+    for filename in os.listdir(sounds_directory):
         if filename.endswith('.wav'):
-            sounds.append(f'[{filename[:-4]}]')
+            fullpath = f'{sounds_directory}/{filename}'
+
+            # Check number of channels and sample rate
+            if sox.file_info.channels(fullpath) == 1 & int(sox.file_info.sample_rate(fullpath) == 22050.0):
+                sounds.append(f'[{filename[:-4]}]')
+            else:
+                log.error(f'File {fullpath} is not mono channel or has wrong samplerate.')
+
+
     log.info(f'Succesfully loaded all sounds - {len(sounds)} sounds')
     return sounds
